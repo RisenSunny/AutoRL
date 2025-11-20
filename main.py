38af0ded34c1e9714c6020c5165140edf62f0286
RisenSunny <witaeyang617@gmail.com>
2025-11-20 10:50:57 +0900

    lobby_ui

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import car_rl
 image_height = 900
 image_width = 1600
 
+scene = 0 # 0:lobby, 1:desc, 2:play
+
 # 글로벌 변수들
 drawing = False  # 드래그 상태를 추적하는 변수
 start_point = None  # 드래그 시작 점
@@ -32,16 +34,39 @@ def draw_circle(event, x, y, flags, param):
         # 원 정보 저장
         circles.append((start_point + [radius]))
 
+def lobby_mouse(event,x,y,flags,param):
+    if event ==cv2.EVENT_LBUTTONUP:
+        print("test")
+
 # 빈 이미지 생성
 image = np.full((image_height, image_width, 3), 255,dtype=np.uint8)
+
+#로비 화면 생성
+lobby_image = np.full((image_height, image_width, 3), 255,dtype=np.uint8)
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 5
+thickness = 10
+textsize, baseline = cv2.getTextSize("AutoRL", font, font_scale, thickness)
+cv2.putText(lobby_image, "AutoRL",((1600 - textsize[0])//2, 300), font, font_scale, (0,0,0), thickness)
+
+button_size = (600, 150)
+button_y_pos = 450
+cv2.rectangle(lobby_image, (800 - button_size[0]//2, button_y_pos),(800 + button_size[0]//2, button_y_pos+button_size[1]), (0,0,0), 5, )
+
+button_y_pos = 650
+cv2.rectangle(lobby_image, (800 - button_size[0]//2, button_y_pos),(800 + button_size[0]//2, button_y_pos+button_size[1]), (0,0,0), 5, )
 
 # 윈도우 생성 및 콜백 함수 등록
 cv2.namedWindow('car-rl')
 cv2.setMouseCallback('car-rl', draw_circle)
+cv2.setMouseCallback('car-rl', lobby_mouse)
 
 while True:
     # 이미지 복사본 생성
-    img_copy = image.copy()
+    if scene==0:
+        img_copy = lobby_image.copy()
+    elif scene ==2:
+        img_copy = image.copy()
 
     # 저장된 모든 동그라미 그리기
     for circle in circles:
